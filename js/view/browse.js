a054379fbbd37dce089d715d303e3f0fd573eb54
Kent Tam <ktam39@gmail.com>
2012-10-08 22:04:28 -0700

    Queued the makelinkactive functions to make sure links already populated

--- js/view/browse.js
+++ js/view/browse.js
@@ -26,112 +26,34 @@ $("#search-form").click(function(event){
 	 event.stopPropagation();
 });
 
+Sammy(function() {
+	//gotolevel2
+    this.get(/\#(.*)/, function(){ 
+    	var route = this.params.splat[0];
+    	var level = route.split("/").length;  // this would tell me which level I'm on
+    	browse.goToLevel(route, level, false);
 
-// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
-	//Map is the array of folders and files passed from the model
+    	var currentLevels = route.split("/");
+    	var lastHashArray = browse.lastHash.split("/");
 
-function FolderViewModel() {
-    // Data
-    var self = this;
+    	if(currentLevels.length > 1 &&currentLevels[currentLevels.length-2] != lastHashArray[currentLevels.length-2]){
+    		var subroute = route.substr(0, route.length - currentLevels.last().length);
+    		browse.goToLevel(subroute, level-1, false);
+    	}
 
-    self.folders = ko.observableArray(result.directories);
-    self.files = ko.observableArray(result.files);
-    self.firstLevelActive = ko.observable();
-
-    self.secondLevelFolders = ko.observableArray();
-	self.secondLevelFiles = ko.observableArray();
-	self.secondLevelActive = ko.observable();
-
-    self.thirdLevelFolders = ko.observableArray();
-	self.thirdLevelFiles = ko.observableArray();
-	self.thirdLevelActive = ko.observable();
-
-	self.fourthLevelFolders = ko.observableArray();
-	self.fourthLevelFiles = ko.observableArray();
-
-	self.fourthLevelVisible = ko.observable();
-
-	self.queryString = ko.observable("");
-	self.searchResultsDirectories = ko.computed(function(){
-		if(self.queryString() != ""){
-			$.post(base_url+'/browse/search', { 'term' :  self.queryString()}, function(data){
-	        	var s_data = jQuery.parseJSON(data);
-	        	self.searchResultsFiles(s_data.files);
-	        	self.searchResultsFolders(s_data.directory);
-	        	return (s_data.directory);
-	        	//console.log($(this).children().first().val());
-	        });
-		}
-		/*else{
-			self.searchResultsFiles();
-	        self.searchResultsFolders();
-		}*/
-	});
-	self.searchResultsFiles = ko.observableArray();
-	self.searchResultsFolders = ko.observableArray();
-	self.resulsExists = ko.computed(function(){
-		//positionSearchResults();
-		if((self.searchResultsFolders().length + self.searchResultsFiles().length > 0) && (self.searchIsSelected()) && (self.queryString() != "")){
-			return true;
-		}
-		else
-			return false;
-	});
-
-	self.searchIsSelected = ko.observable(false);
-
-
-    // Behaviours    
-    self.goToLevels = function(folder) { 
-		location.hash = folder.path;	
-    };
-
-    self.goToLevel4 = function(folder) { 
-        //alert(folder.path);
-        $.get('<?php echo base_url();?>index.php/browse/directory_map', {  'directory' :  folder.path}, function(data){
-        	var s_data = jQuery.parseJSON(data);
-        	self.fourthLevelFolders(s_data.directories);
-        	self.fourthLevelFiles(s_data.files);
-        	//alert(d);
-        });
-    };
-
-
-    // Client-side routes
-	Sammy(function() {
-		//gotolevel2
-        this.get(/\#(.*)/, function(){ 
-        	var route = this.params.splat[0];
-        	var level = route.split("/").length;  // this would tell me which level I'm on
-        	browse.goToLevel(route, level, false);
-
-        	var currentLevels = route.split("/");
-        	var lastHashArray = browse.lastHash.split("/");
-
-
-
-        	if(currentLevels.length > 1 &&currentLevels[currentLevels.length-2] != lastHashArray[currentLevels.length-2]){
-        		var subroute = route.substr(0, route.length - currentLevels.last().length);
-        		browse.goToLevel(subroute, level-1, false);
-        	}
-
-        	if(browse.currentLevel < level && level >= 3){ //added a level
-        		browse.createNewLevel();
-				browse.shiftAllLevels();
-        	}
-        	else if (browse.currentLevel > level && level >= 2){ //removed a level
-        		browse.firstLevelClick();
-        	}
-        	browse.currentLevel = level;
-        	browse.lastHash = route;
-        	//browse.changeLocation();	
-        });
-    	
-    }).run(); 
-
-};
-
-ko.applyBindings(new FolderViewModel());
+    	if(browse.currentLevel < level && level >= 3){ //added a level
+    		browse.createNewLevel();
+			browse.shiftAllLevels();
+    	}
+    	else if (browse.currentLevel > level && level >= 2){ //removed a level
+    		browse.firstLevelClick();
+    	}
+    	browse.currentLevel = level;
+    	browse.lastHash = route;
+    	//browse.changeLocation();	
+    });
+	
+}).run(); 
 
 function undoFourth(){
 	if($('#fourth-level').css('display') == "block"){
@@ -145,7 +67,9 @@ var browse = {
 	folder : false,
 	currentLevelContainer: null,
 	currentLevel: 0,
+	returned:0,
 	lastHash : window.location.hash,
+	levelQueue : $({}),
 	init: function(){
 		this.initializeLevels();
 		$(".level-container").on("click", ".directory-link", this.changeLocation);
@@ -163,9 +87,6 @@ var browse = {
 				continue;
 			}
 
-			//var folderClicked = hashArray[x];
-			//$(".level-container").eq(x).find("a[data-path='"+folderClicked+"']").parent().addClass("active");
-			
 			if(x+1 > 2){
 				this.createNewLevel();
 				this.shiftAllLevels();
@@ -184,10 +105,22 @@ var browse = {
     		if(route.substr(-1) != "/"){ //to make data consistent
     			route+="/";
     		}
-    		$(".level-container").eq(level-1).find(".active").removeClass("active");
-			$(".level-container").eq(level-1).find("a[data-path='"+route.substr(0, route.length-1)+"']").parent().addClass("active");
-
+    		browse.levelQueue.queue('level',function(next){
+    			browse.makeLinkActiveCallback(route, level);
+    			next();
+    		});
+    		browse.returned++;
+    		if(browse.returned === $(".level-container").length){
+    			browse.levelQueue.dequeue('level');
+    			browse.returned = 0;
+    		}
+    		/*callbacks.add(browse.makeLinkActiveCallback);
+    		callbacks.fire([route, level]);*/
     	});
+	},
+	makeLinkActiveCallback: function(route, level){
+	    $(".level-container").eq(level-1).find(".active").removeClass("active");
+		$(".level-container").eq(level-1).find("a[data-path='"+route.substr(0, route.length-1)+"']").parent().addClass("active");
 	},
 	changeLocation: function(){
 		location.hash = $(this).attr("data-path");
@@ -256,32 +189,6 @@ var browse = {
 			$levelContainer.eq(numLevels-1).html("");
 		}
 
-	},
-	removeHigherLevel: function(currentLevelContainer){
-		//only happens by clicking on first
-		//only need to clear third level
-
-
-
-		/*var $levelContainer = $(".level-container");
-		var currentLevel = $levelContainer.index(currentLevelContainer);
-		var levelCount = $levelContainer.length;
-		for (var x = currentLevel+1; x < levelCount; x++){
-			$levelContainer.eq(x).html("");
-		}
-
-		if(levelCount > 3 && currentLevel <= levelCount - 1){ 
-			//must have levels folded
-			//the current level will have to go to the second slot
-			var lastValidLevel = currentLevelContainer.animate({"left": "600px"})
-				.next().animate({"left": "900px"});
-			var lastIndex = currentLevel+1;
-			for(var x = levelCount; x > currentLevel + 1; x--){
-				$levelContainer.last().remove();
-			}
-		}*/
-			
-		//slide levels back if needed
 	}
 }
 	
